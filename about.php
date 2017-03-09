7a67d9c24a6db811474a9d37fb2d217af54dcebe
Tyson Elford <tyelford@gmail.com>
2017-03-09 17:04:30 +1000

    Contact Us done about started

--- about.php
+++ about.php
@@ -1,28 +1,118 @@
 <?php
-	//Set the page title
-	$title = 'About Us | ICF Builders Inc.';
-	$customStylesheet = "<link rel='stylesheet' href='styles/about.css'>";
-	//Header portion of the page
-	include 'header.php';
-	/*PAGE MAIN CONTENT*/
+//Set the page title
+$title = 'About Us | ICF Builders Inc.';
+$customStylesheet = "<link rel='stylesheet' href='styles/about.css'>";
+//Header portion of the page
+include 'header.php';
+/*PAGE MAIN CONTENT*/
 ?>
 
-	<div class='container'>
-	  
-        
+<div class='container'>
 
-  
+    <div class="text-center">
+        <h2>About ICF Builders</h2>
+        <p>Put a bunch of text here about what the company does / is</p>
+    </div>
 
-	</div>
-	
-	
-	
-	
-	
-	
+    <div class="text-center">
+        <h2>Meet the ICF Team</h2>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-8">
+            <div class="card about-card">
+                <div class="row">
+                    <div class="col-xs-6">
+                        <h3>Matt Blackmore</h3>
+                        <h4><em>President / CEO</em></h4>
+                        <p>I have been working in this industry for way too long.  I am now totally awesome at it and you can all go to hell.</p>
+                    </div>
+                    <div class="col-xs-6">
+                        <img src="images/defaultAboutUs.jpg" />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-sm-4">
+        </div>
+    </div>
+
+
+     <div class="row">
+         <div class="col-sm-4">
+        </div>
+        <div class="col-sm-8">
+            <div class="card about-card">
+
+                <div class="row">
+                    <div class="col-xs-6">
+                        <img src="images/defaultAboutUs.jpg" />
+                    </div>
+                    <div class="col-xs-6">
+                        <h3>Jake Elford</h3>
+                        <h4><em>Site Supervisor / Principal</em></h4>
+                        <p>I have been working in this industry for way too long.  I am now totally awesome at it and you can all go to hell.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-8">
+            <div class="card about-card">
+                <div class="row">
+                    <div class="col-xs-6">
+                        <h3>Murry Blackmore</h3>
+                        <h4><em>Community Manager</em></h4>
+                        <p>I have been working in this industry for way too long.  I am now totally awesome at it and you can all go to hell.</p>
+                    </div>
+                    <div class="col-xs-6">
+                        <img src="images/defaultAboutUs.jpg" />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-sm-4">
+        </div>
+    </div>
+
+    <div class="row">
+         <div class="col-sm-4">
+        </div>
+        <div class="col-sm-8">
+            <div class="card about-card">
+
+                <div class="row">
+                    <div class="col-xs-6">
+                        <img src="images/defaultAboutUs.jpg" />
+                    </div>
+                    <div class="col-xs-6">
+                        <h3>Jared Moss</h3>
+                        <h4><em>Community Manager</em></h4>
+                        <p>I have been working in this industry for way too long.  I am now totally awesome at it and you can all go to hell.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+
+</div>
+
+
+
+
+
+
 <?php	
-	/*END OF PAGE CONTENT*/
-	//Foot portion of the page
-	include 'footer.php';
+/*END OF PAGE CONTENT*/
+//Foot portion of the page
+include 'footer.php';
 
 ?>
--- contact.php
+++ contact.php
@@ -7,22 +7,80 @@
 	/*PAGE MAIN CONTENT*/
 ?>
 <?php
-$action = $_REQUEST['action'];
-if($action == "new"){
-    //Display the form
-    ?>
+$result = "";
+$isQuoteRequest = 0;
+if(isset($_GET['c'])){
+    //Quote request, change the drop down to quote request
+    if($_GET['c'] == "quote"){
+        $isQuoteRequest = 1;
+    }
+}
+
+if(isset($_POST["submit"])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $query = $_POST['queryType'];
+
+    $to = 'tyelford@gmail.com';
+
+    $body  = "From: $name\n Email: $email Message: $message";
+
+    $errName = "";
+    $errEmail = "";
+    $errMessage = "";
+
+    //Check if the name has been entered
+    if(!$_POST['name']){
+        $errName = 'Please enter your name';
+    }
+
+    // Check if email has been entered and is valid
+    if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $errEmail = 'Please enter a valid email address';
+    }
+
+    //Check if message has been entered
+    if (!$_POST['message']) {
+        $errMessage = 'Please enter your message';
+    }
+
+    $subject = "";
+
+    if($query == "general"){
+        $subject = "[General Query] ";
+    }
+    if($query == "quote"){
+        $subject = "[Quote Request] ";
+    }
+    if($query == "comp"){
+        $subject = "[Compliment] ";
+    }
+    
+    $subject = $subject . "from the Website";
+
+    //If there are no errors then send the message
+    if(!$errName && !$errEmail && !$errMessage){
+        if(/*mail ($to, $subject, $body, $from)*/true){
+            $result = '<div class="alert alert-success">Thank You! We will be in touch</div>';
+        }else{
+            $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later.</div>';
+        }
+    }
+}
+?>
 
 	<div class='container'>
 	  
         <h2 class="text-center">Contact Us</h2>
 
-        <form action="new" method="post" enctype="multipart/form-data">
+        <form action="contact.php" method="post">
             <div class="form-group">
                 <label>Type of Query</label>
-                <select class="form-control">
-                    <option>General Question</option>
-                    <option>Quote Request</option>
-                    <option>Compliment</option>
+                <select class="form-control" name="queryType">
+                    <option value="general">General Question</option>
+                    <option value="quote" <?php if($isQuoteRequest == 1){echo " selected";}?> >Quote Request</option>
+                    <option value="comp">Compliment</option>
                 </select>
             </div>
             
@@ -42,33 +100,15 @@ if($action == "new"){
             </div>
 
             <div class="form-group">
-                <button type="submit" class="btn btn-default">Send Message</button>
+                <input type="submit" name="submit" value="Send Message" id="submit" class="btn btn-default" />
+            </div>
+
+            <div class="form-group">
+                <?php echo $result; ?>
             </div>
         </form>
 
 	</div>
-	
-	<?php
-}
-else{
-    //Send the submitted data
-    $name = $_REQUEST['name'];
-    $email = $_REQUEST['email'];
-    $message = $_REQUEST['message'];
-
-
-    $from = "From: $name<$email>\r\n";
-    $subject = "Message from the website";
-    $to = "tyelford@gmail.com";
-    $headers = $from . 
-                'Reply-To: ' . $email . '\r\n' .
-                'X-Mailer: PHP/' . phpversion();
-
-    mail($to, $subject, $message, $headers);
-    echo "Email Sent";
-}
-	
-	?>
 	
 	
 <?php	
